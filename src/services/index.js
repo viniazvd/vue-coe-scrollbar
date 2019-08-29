9de66f1a90b0772c6cb60e7b1d2dcb9fe7bf28ed
Vinicius Azevedo <vinicius.azevedo@convenia.com.br>
2019-08-29 19:03:00 -0300

    :fire: remove isMobile fn

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,3 @@
-export function isMobile () {
-  return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
-}
-
 // ty @vinicius73
 export function bindEvent (el, event, callback, ...options) {
   el.addEventListener(event, callback, ...options)
