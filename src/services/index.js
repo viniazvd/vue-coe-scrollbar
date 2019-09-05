35913f0ea0831d40d46e5040b9092e2905764442
Vinicius Azevedo <vinicius.azevedo@convenia.com.br>
2019-09-05 19:06:37 -0300

    :tada: init #easeInOutQuad

--- src/services/index.js
+++ src/services/index.js
@@ -4,3 +4,14 @@ export function bindEvent (el, event, callback, ...options) {
 
   return () => el.removeEventListener(event, callback, ...options)
 }
+
+// ty @giseudo
+export function easeInOutQuad (time, start, change, duration) {
+  time /= duration / 2
+
+  if (time < 1) return change / 2 * time * time + start
+
+  time--
+
+  return -change / 2 * (time * (time - 2) - 1) + start
+}
