2d9cc38a290327591748c850b9d7957b93f82348
Giseudo Oliveira <giseudo.oliveira@convenia.com.br>
2019-09-06 15:38:24 -0300

    :art: Improved examples with some cards

--- examples/App.vue
+++ examples/App.vue
@@ -1,15 +1,14 @@
 <template>
   <main class="App">
     <h1 class="App__title">
-      <code class="App__name">{{ name }}</code>
-    </h1>
-
-    <vue-coe-scroll>
+      <span class="App__name">{{ name }}</span>
       <button @click="content.push('kkk')">add</button>
       <button @click="content.pop()">remove</button>
-      <span>n contents {{ content.length }}</span>
+      <span class="App__total">n contents {{ content.length }}</span>
+    </h1>
 
-      <div v-for="(x, i) in content" :key="i" class='list'>{{ x }}</div>
+    <vue-coe-scroll class="List" :scrollbar-width="12">
+      <div v-for="(x, i) in content" :key="i" class="List__item"></div>
     </vue-coe-scroll>
   </main>
 </template>
@@ -31,16 +30,43 @@ export default {
 </script>
 
 <style lang="scss">
+* { box-sizing: border-box; }
+
+html, body {
+  margin: 0;
+  border: 0;
+  padding: 0;
+  height: 100%;
+  font-family: Arial;
+  overflow: hidden;
+
+  @media only screen and (max-width: 575px) { overflow: visible; }
+}
+
 .App {
   height: 100%;
   display: flex;
   flex-direction: column;
 
   &__title { font-size: 28px; }
+  &__name { display: block; }
 }
 
-.list {
-  padding: 25vh 50px;
-  border: 1px solid transparent;
+.List {
+  & > .content {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-column-gap: 10px;
+    grid-row-gap: 10px;
+    padding: 10px;
+  }
+  &__item {
+    background: red;
+    &:after{
+      content: '';
+      display: block;
+      padding-top: 100%;
+    }
+  }
 }
 </style>
